2c5003e487d372fb86bd3400e2b694e3c1a98245
Vladislav <VladislavPozhar@yandex.ru>
2023-04-06 17:39:40 +0400

    bug fix

--- ForumBackend/src/main/java/com/Eteryz/ForumBackend/models/Article.java
+++ ForumBackend/src/main/java/com/Eteryz/ForumBackend/models/Article.java
@@ -27,7 +27,7 @@ public class Article {
     @Column(name = "articles_id")
     private String id;
     private String title;
-    @Lob
+    @Type(type = "org.hibernate.type.TextType")
     private String text;
     @Formula(value = "(select count(a.users_id) from article_rating a where a.status = true and a.articles_id = articles_id)")
     private int likes;
